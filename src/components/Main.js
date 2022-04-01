a809e62fa6e6c0fa24bf102354b47fa8fedc158c
AlanNJ <alannpn75@gmail.com>
2022-04-01 22:43:39 +0530

    second-commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+
+export const Main = () => {
+	return (
+		<>
+			<div className="h-auto flex flex-col justify-center items-center">
+				<Card />
+				<Card />
+			</div>
+		</>
+	);
+};
+export default Main;
